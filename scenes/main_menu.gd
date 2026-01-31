3942d3094c3e1a495d0d82ceeb78c3b8975efe1e
VictorMatheuz <victor.matheus@viaconsulting.com.br>
2026-01-31 18:28:02 -0300

    Dialogic corrigido

---
+++ scenes/main_menu.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_novo_jogo_pressed() -> void:
+	Dialogic.timeline_ended.connect(ended)
+	Dialogic.start("ato1")
+	set_physics_process(false)
+	
+
+func ended():
+	Dialogic.timeline_ended.disconnect(ended)
+	set_physics_process(true)
+
+
+func _on_sair_pressed() -> void:
+	get_tree().quit()
